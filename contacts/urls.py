735335186cb41b508d0b0c76ce1ea7f2b1ec38de
Faisal Jawad <faisal.jawad@systemsltd.com>
2021-12-21 14:49:51 +0500

    Added contact routes

---
+++ contacts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import ContactsList, ContactsDetailView
+
+
+urlpatterns = [
+    path('', ContactsList.as_view()),
+    path('<int>:id>', ContactsDetailView.as_view()),
+]
--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Contact
+from rest_framework import permissions
+from .serializers import ContactSerializer
 # Create your views here.
+
+class ContactsList(ListCreateAPIView):
+    serializer_class = ContactSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+    def create(self, serializer):
+        serializer.save(owner=self.request.user)
+        
+    def get_queryset(self):
+        return Contact.objects.filter(owner=self.request.user)
+
+
+class ContactsDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class = ContactSerializer
+    permission_classes = (permissions.IsAuthenticated)
+    lookup_field = "id" # /contacts/1 (search like this)    
+    
+    def get_queryset(self):
+        return Contact.objects.filter(owner=self.request.user)
