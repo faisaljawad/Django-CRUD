825f5b9c64d4e41c2044e236deefebf6e9174ebf
Faisal Jawad <faisal.jawad@systemsltd.com>
2021-12-21 14:22:57 +0500

    added new migration for contacts table

--- contacts_api/settings.py
+++ contacts_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'contacts',
     'rest_framework',
     'authentication',
     'rest_framework_simplejwt',
